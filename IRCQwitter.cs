2c783e2e0429f1323c10f4994c910e48f2c54677
hfukada <hiroshi@fukada.us>
2013-09-02 01:34:00 -0400

    First commit, just struct

---
+++ IRCQwitter.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+
+namesplace IRCQwitter.cs
+{
+    internal struct IRCConfig{
+        public string server;
+        public int port;
+        public string nick;
+        public string name;
+    }
+}
